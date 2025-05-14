1f806bf43923a536d2e3dd3527c9f0180d623690
Seth-Smith88 <mrsmith2012@live.co.uk>
2025-05-15 00:37:43 +0100

    enremy script updated

--- Assets/Seth's Dungeon/Scripts/Enemies/Floating Health bar.cs
+++ Assets/Seth's Dungeon/Scripts/Enemies/Floating Health bar.cs
@@ -5,12 +5,7 @@ using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 { 
-    [SerializeField] private Slider slider;
     
-    public void UpdateHealthBar(float.currentValue, float.maxValue);
-    {
-        slider.value = currentValue / maxValue
-    }
 
     // Update is called once per frame
     void Update()
